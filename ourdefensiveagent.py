4fd2771514c27abe59c324e5cd0f4fac75f6a6d8
Kali <ets147@msstate.edu>
2022-10-30 13:07:17 -0400

    removed unusued functions

--- ourdefensiveagent.py
+++ ourdefensiveagent.py
@@ -21,47 +21,37 @@ from ray.rllib.agents.ppo import PPOTrainer
 from ray.rllib.models import ModelCatalog
 
 class ourdefensiveagent(BaseAgent):
-    # agent that loads a StableBaselines3 PPO model file
-    def train(self, results):
-        pass
 
     def end_episode(self):
-        pass
+        self.set = False
 
-    def set_initial_values(self, action_space, observation):
-        pass
-
+    #constructor for the agent object, made with reference to the mindrake implementation.
     def __init__(self, model_file: str = None):
+
+        #configuration
         ModelCatalog.register_custom_model("CybORG_hier_Model", TorchModel)
         RM_config = meander_config
         RM_config["in_evaluation"] = True
         RM_config["explore"] = False
 
-        self.RM_def = PPOTrainer(config=RM_config, env=CybORGAgent)
-        self.RM_def.restore("./checkpoint-1829")
-
-
+        #variable intialization
         self.state = [np.zeros(256, np.float32),
                       np.zeros(256, np.float32)]
         self.step = 0
-        # heuristics
         self.set = False
         self.observations = []
         self.adversary = 0
 
+        #Loads the weights from mindrakes meander implementation
+        self.RM_def = PPOTrainer(config=RM_config, env=CybORGAgent)
+        self.RM_def.restore("./checkpoint-1829")
+
+
+    #Feeds in state to neural network, which outputs an action which it performs.
     def get_action(self, obs, action_space):
         agent_action, state, _ = self.RM_def.compute_single_action(obs[2:], self.state)
         # print('meander defence')
         self.state = state
         return agent_action, self.adversary
 
-        """
-        if self.model is None:
-            path = str(inspect.getfile(CybORG))
-            path = path[:-10] + '/Shared/Scenarios/Scenario1b.yaml'
-            cyborg = ChallengeWrapper(env=CybORG(path, 'sim'), agent_name='Blue')
-            #self.model = PPO('MlpPolicy', cyborg)
-        #action, _states = self.model.predict
-        action = 0
-        return action
-        """
+
